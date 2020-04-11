bcb1915dd3a337899bb430e1e0d9b530307e96e9
Locke <amielph@gmail.com>
2020-04-11 18:41:49 -0400

    Functional setup.py. Tested as function in python 2.

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
-
 setuptools.setup(
-     name='sonipy',  
+     name='sonipy',
      version='0.1',
-     scripts=['sonipy'] ,
-     author="Locke Patton",
-     author_email="locke.patton@cfa.harvard.edu",
-     description="Sonification of 2D plots",
+     author='Locke Patton',
+     author_email='locke.patton@cfa.harvard.edu',
+     description='Sonification of 2D plots',
      long_description=long_description,
-   long_description_content_type="text/markdown",
-     url="https://github.com/lockepatton/sonipy",
-     packages=setuptools.find_packages(),
+     long_description_content_type='text/markdown',
+     url='https://github.com/lockepatton/sonipy',
+     packages=['sonipy'],
      classifiers=[
-         "Programming Language :: Python :: 3",
-         "License :: OSI Approved :: MIT License",
-         "Operating System :: OS Independent",
+         'Programming Language :: Python :: 2',
+         'License :: OSI Approved :: MIT License',
+         'Operating System :: OS Independent',
      ],
+     install_requires=['numpy', 'matplotlib', 'pandas', 'pydub',
+                       'repo @ https://github.com/AllenDowney/ThinkDSP.zip#egg=repo-1.0.0'],
  )
